aaf1712cf4eaa47edfcaeef10989fba0237c9491
CalebEhlers <122182878+CalebEhlers@users.noreply.github.com>
2024-01-20 23:01:48 -0600

    setup ccd clocking signal, ready to test

--- Raman_Software/Raman_Software.ino
+++ Raman_Software/Raman_Software.ino
@@ -1,13 +1,35 @@
 #include "Raman_Software.h"
+#include <cmath>
 
 #define BAUDRATE 0
 
-#define CLK_FREQUENCY 2000000.0 //Hz
-#define CLK_DURATION (1/CLK_FREQUENCY) //seconds
+#define CLK_FREQUENCY 1000000.0 //Hz
+#define CLK_TIMEPERIOD (1.0 / CLK_FREQUENCY) //seconds
+#define SEQUENCE_DURATION //TODO
 
-#define ROG_PULSE_PERIOD 5000 //ns
+#define ROG_t7 5000 //ns
+#define CLK_t5 3000
+#define CLK_t9 3000
 
-bool startAquisitionTimestamp = 0;
+#define PIXEL_COUNT 2048
+#define CLK_REPETITIONS 2088
+#define TEENSY_TIMETOHIGH 0//TODO measure
+#define TEENSY_TIMETOLOW 0 //TODO measure
+
+#define SH_MODE true//TODO make both options
+
+double startResetTimestamp = 0;
+double startAquiringTimestamp = 0;
+
+
+int pixelArray[PIXEL_COUNT];
+
+enum ClockingState
+{
+    idling, resetting, aquiring
+};
+
+ClockingState clockingState = idling;
 
 double getTimestamp(){
     return ((float) millis()) / 1000.0;
@@ -28,7 +50,6 @@ void setup(){
 
 void loop(){
 
-    //float timestamp = getTimestamp();
 
 /*
     read signal from rovecomm:
@@ -40,10 +61,11 @@ void loop(){
 */
 
 
+    float timestamp = getTimestamp();
     //variables to read from rovecomm
     bool green = false;
     bool red = false;
-    bool aquireData = true;
+    bool aquireData = true; //TODO maybe have an integer representing the amount of aquisitions to take, then run the ccd for that many aquisitions, then average them, and set the ccd to idle when finished
 
 
 
@@ -60,22 +82,53 @@ void loop(){
 
 
     if(aquireData){
-        if(beginning of clock sequence){
-            startAquisitionTimestamp = getTimestamp();
+        if(clockingState == idling){ //if we are idling and we want to aquire data, we reset
+            startResetTimestamp = getTimestamp();
+            clockingState = resetting;
         }
 
-        //logic for CLK
+        if(clockingState = resetting){ // the resetting state is the dip in the ROG before the normal clocking signal
+            //logic for the reset sequence at the beginning
+            if(timestamp < startResetTimestamp + CLK_t5){
+                digitalWrite(CLK, HIGH);
+                digitalWrite(ROG, HIGH);
+            } else if(timestamp < startResetTimestamp + CLK_t5 + ROG_t7){
+                digitalWrite(CLK, HIGH);
+                digitalWrite(ROG, LOW);
+            } else if(timestamp < startResetTimestamp + CLK_t5 + ROG_t7 + CLK_t9){
+                digitalWrite(CLK, HIGH);
+                digitalWrite(ROG, HIGH);
+            } else{
+                startAquiringTimestamp = getTimestamp();
+                clockingState = aquiring;
+            }
+        }
+        
 
 
-        //logic for ROG
 
+        if(clockingState = aquiring){ 
+            //logic for CLK
+                // (timestamp-startAquiringTimestamp)/CLK_TIMEPERIOD == number of cycles of the clock
+                // floor((timestamp-startAquiringTimestamp)/CLK_TIMEPERIOD) == number of cycles fully complete(rounded down)
+                // floor((timestamp-startAquiringTimestamp)/CLK_TIMEPERIOD) * CLK_TIMEPERIOD == the start time of the latest cycle
+                // timestamp - floor((timestamp-startAquiringTimestamp)/CLK_TIMEPERIOD) * CLK_TIMEPERIOD == how far we are into the current cycle
+                // timestamp - floor((timestamp-startAquiringTimestamp)/CLK_TIMEPERIOD) * CLK_TIMEPERIOD < (CLK_TIMEPERIOD/2) == true if we are in the first half of the cycle
+            if( timestamp - floor((timestamp-startAquiringTimestamp)/CLK_TIMEPERIOD) * CLK_TIMEPERIOD < (CLK_TIMEPERIOD/2)){ //if we are at the first half of the period, set LOW
+                digitalWrite(CLK, LOW);
+            } else {//in the second half, set HIGH
+                digitalWrite(CLK, HIGH);
+            }
 
-        //logic for 
+            //logic for ROG
+            digitalWrite(ROG, HIGH);
+        }
 
     } else{// if the ccd is idling
-        digitalWrite(CLK, HIGH);
+
+        digitalWrite(CLK, LOW);
         digitalWrite(ROG, LOW);
-        digitalWrite()
+        delay(1000); //TODO REMOVE THIS, THIS IS TO ADD A DELAY IN BETWEEN RUNS FOR TESTING
     }
 
 
