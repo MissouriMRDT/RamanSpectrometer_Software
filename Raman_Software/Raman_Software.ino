046eecae34b1b31dc9d950dc48e1faa999b1fcb3
CalebEhlers <122182878+CalebEhlers@users.noreply.github.com>
2024-02-04 12:17:08 -0600

    finish ccd setup

--- Raman_Software/Raman_Software.ino
+++ Raman_Software/Raman_Software.ino
@@ -3,7 +3,6 @@
 
 void setup(){
     Serial.begin(BAUDRATE);
-    Serial.println("setup");
 
     pinMode(CLK_OUT, OUTPUT);
     pinMode(ROG, OUTPUT);
@@ -16,11 +15,9 @@ void setup(){
     
     analogWriteFrequency(CLK_OUT, CLK_FREQUENCY);
     analogWrite(CLK_OUT, 0);//50% duty cycle
-    Serial.println(CLK_TIMEPERIOD*CLK_REPETITIONS*1000.0);
-
     
     Serial.println("RoveComm Initializing...");
-    RoveComm.begin(RC_ARMBOARD_FIRSTOCTET, RC_ARMBOARD_SECONDOCTET, RC_ARMBOARD_THIRDOCTET, RC_ARMBOARD_FOURTHOCTET, &TCPServer);
+    RoveComm.begin(RC_RAMANSPECTROMETERBOARD_FIRSTOCTET, RC_RAMANSPECTROMETERBOARD_SECONDOCTET, RC_RAMANSPECTROMETERBOARD_THIRDOCTET, RC_RAMANSPECTROMETERBOARD_FOURTHOCTET, &TCPServer);
     Serial.println("Complete");
 }
 
@@ -28,21 +25,22 @@ void setup(){
 void loop(){
     //watchdog setup to turn off lights if there is no communication???
 
+    readCCD();
+
     rovecomm_packet packet = RoveComm.read();
     switch (packet.data_id) {
 
-        case 8000:
+        case RC_RAMANSPECTROMETERBOARD_REQUESTREADING_DATA_ID:
         {
-            readCCD();
-            RoveComm.write(8010, 500, &pixelArray[0]);
-            RoveComm.write(8011, 500, &pixelArray[500]);
-            RoveComm.write(8012, 500, &pixelArray[1000]);
-            RoveComm.write(8013, 500, &pixelArray[1500]);
-            RoveComm.write(8014, 48, &pixelArray[2000]);
+            RoveComm.write(RC_RAMANSPECTROMETERBOARD_CCDREADING_PART1_DATA_ID, 500, &pixelArray[0]);
+            RoveComm.write(RC_RAMANSPECTROMETERBOARD_CCDREADING_PART2_DATA_ID, 500, &pixelArray[500]);
+            RoveComm.write(RC_RAMANSPECTROMETERBOARD_CCDREADING_PART3_DATA_ID, 500, &pixelArray[1000]);
+            RoveComm.write(RC_RAMANSPECTROMETERBOARD_CCDREADING_PART4_DATA_ID, 500, &pixelArray[1500]);
+            RoveComm.write(RC_RAMANSPECTROMETERBOARD_CCDREADING_PART5_DATA_ID, 48, &pixelArray[2000]);
 
         }
 
-        case 8001:
+        case RC_RAMANSPECTROMETERBOARD_ENABLELEDS_DATA_ID:
         {
             uint8_t data = *((uint8_t*) packet.data);
             digitalWrite(GREEN, (data & 1<<0)? HIGH : LOW);
@@ -56,29 +54,36 @@ void loop(){
 
 void initADC() {
     // idk what this does
-	CCM_CCGR1 |= CCM_CCGR1_ADC1(CCM_CCGR_ON);
+	  CCM_CCGR1 |= CCM_CCGR1_ADC1(CCM_CCGR_ON);
+	  CCM_CCGR1 |= CCM_CCGR1_ADC2(CCM_CCGR_ON);
 	
     // 4.121 us
-	// 10 bit conversion (17 clocks) plus 20 clocks for input settling
+	  // 10 bit conversion (17 clocks) plus 20 clocks for input settling
     // async clock
-	ADC1_CFG = ADC_CFG_MODE(1) | ADC_CFG_ADSTS(2) | ADC_CFG_ADLSMP | ADC_CFG_ADIV(1) | ADC_CFG_ADICLK(3) | ADC_CFG_ADHSC;
+	  ADC1_CFG = ADC_CFG_MODE(1) | ADC_CFG_ADSTS(2) | ADC_CFG_ADLSMP | ADC_CFG_ADIV(1) | ADC_CFG_ADICLK(3) | ADC_CFG_ADHSC;
+	  ADC2_CFG = ADC_CFG_MODE(1) | ADC_CFG_ADSTS(2) | ADC_CFG_ADLSMP | ADC_CFG_ADIV(1) | ADC_CFG_ADICLK(3) | ADC_CFG_ADHSC;
 
     // 3.385 us
     // 8 bit conversion (17 clocks) plus 8 clocks for input settling
     // async clock
-	// ADC1_CFG = ADC_CFG_MODE(0) | ADC_CFG_ADSTS(3) | ADC_CFG_ADIV(1) | ADC_CFG_ADICLK(3) | ADC_CFG_ADHSC;
+	  // ADC1_CFG = ADC_CFG_MODE(0) | ADC_CFG_ADSTS(3) | ADC_CFG_ADIV(1) | ADC_CFG_ADICLK(3) | ADC_CFG_ADHSC;
+    // ADC2_CFG = ADC_CFG_MODE(0) | ADC_CFG_ADSTS(3) | ADC_CFG_ADIV(1) | ADC_CFG_ADICLK(3) | ADC_CFG_ADHSC;
   
     // calibrate
-	ADC1_GC = ADC_GC_CAL;
-	while (ADC1_GC & ADC_GC_CAL);
+	  ADC1_GC = ADC_GC_CAL;
+	  ADC2_GC = ADC_GC_CAL;
+	  while (ADC1_GC & ADC_GC_CAL);
+	  while (ADC2_GC & ADC_GC_CAL);
 }
 
 
 uint16_t pixel_index = 0;
 void CLK_ISR() {
     if(pixel_index >= 33 && pixel_index < 2048+33){
-      delay(CLK_TIMEPERIOD * 0.1);
-        pixelArray[pixel_index-33] = analogRead(CLK_IN);
+        delayNanoseconds(70);
+        ADC2_HC0 = 3;
+        while (!(ADC2_HS & ADC_HS_COCO0));
+        pixelArray[pixel_index-33] = ADC2_R0; //analogRead(CLK_IN);
     }
     pixel_index++;
 }
@@ -103,7 +108,7 @@ void readCCD(){
     attachInterrupt(digitalPinToInterrupt(CLK_IN), CLK_ISR, RISING);
     digitalWrite(ROG, HIGH);
     analogWrite(CLK_OUT, 128);
-    delay(CLK_TIMEPERIOD*CLK_REPETITIONS*1000.0);
+    delay(CLK_TIMEPERIOD*CLK_REPETITIONS*1000.0*1.5);
     detachInterrupt(digitalPinToInterrupt(CLK_IN));
     analogWrite(CLK_OUT, 0);
 
